a0a54cf64a3b315dffb17c78844d78439478741e
Walker-luo <3131luo@sjtu.edu.cn>
2025-05-11 11:32:13 +0800

    上传js代码完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,26 @@
-Page({})
-
+Page({
+    data: {
+      imagePath: "" // 存储选中图片的临时路径
+    },
+    
+    // 点击上传区域时调用的函数
+    chooseImage: function () {
+      wx.chooseMedia({
+        count: 1, // 只允许选择一张图片
+        mediaType: ['image'], // 仅允许选择图片
+        sourceType: ['album', 'camera'], // 可以从相册选择或使用相机拍照
+        success: (res) => {
+          // res.tempFiles 是一个数组，每个元素包含 tempFilePath、size、type 等信息
+          if (res && res.tempFiles && res.tempFiles.length > 0) {
+            this.setData({
+              imagePath: res.tempFiles[0].tempFilePath
+            });
+          }
+        },
+        fail: (err) => {
+          console.error("选择图片失败：", err);
+        }
+      });
+    }
+  });
+  
